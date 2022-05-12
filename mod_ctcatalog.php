cedf682546dec2dc58fcfed57fa5e2f47262ec2e
JoomlaBoat <ivankomlev@gmail.com>
2022-05-11 22:24:27 -0500

    Model independent

--- mod_ctcatalog.php
+++ mod_ctcatalog.php
@@ -1,9 +1,11 @@
 <?php
 /**
  * CustomTables Joomla! 3.x Native Component
- * @author Ivan komlev <support@joomlaboat.com>
+ * @package mod_ctcatalog.php
+ * @author Ivan Komlev <support@joomlaboat.com>
  * @link http://www.joomlaboat.com
- * @license GNU/GPL
+ * @copyright (C) 2018-2022 Ivan Komlev
+ * @license GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
  **/
 
  // no direct access
@@ -17,38 +19,64 @@ $path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 
 
 require_once($path.'loader.php');
 CTLoader();
-    
-$site_path=JPATH_SITE.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.'com_customtables'.DIRECTORY_SEPARATOR.'libraries'.DIRECTORY_SEPARATOR;
 
-$path=JPATH_SITE.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.'com_customtables'.DIRECTORY_SEPARATOR.'libraries'.DIRECTORY_SEPARATOR.'customtables'.DIRECTORY_SEPARATOR;
-require_once($path.'loader.php');
-CTLoader();
+$ct = new CT;
+$ct->Env->menu_params = $params; //$params is the parameter passed by joomla to the module, it contains module settings
 
-require_once(JPATH_SITE.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.'com_customtables'.DIRECTORY_SEPARATOR.'models'.DIRECTORY_SEPARATOR.'catalog.php');
-require_once($site_path.'layout.php');
+if ($ct->Env->legacysupport) {
 
-require_once($site_path.'tagprocessor'.DIRECTORY_SEPARATOR.'catalogtag.php');
-require_once($site_path.'tagprocessor'.DIRECTORY_SEPARATOR.'catalogtableviewtag.php');
+    $site_path=JPATH_SITE.DIRECTORY_SEPARATOR.'components'.DIRECTORY_SEPARATOR.'com_customtables'.DIRECTORY_SEPARATOR.'libraries'.DIRECTORY_SEPARATOR;
 
-require_once($site_path.'tagprocessor'.DIRECTORY_SEPARATOR.'generaltags.php');
-require_once($site_path.'tagprocessor'.DIRECTORY_SEPARATOR.'pagetags.php');
-require_once($site_path.'tagprocessor'.DIRECTORY_SEPARATOR.'itemtags.php');
-require_once($site_path.'tagprocessor'.DIRECTORY_SEPARATOR.'fieldtags.php');
+    require_once($site_path . 'layout.php');
+    require_once($site_path . 'tagprocessor' . DIRECTORY_SEPARATOR . 'catalogtag.php');
+    require_once($site_path . 'tagprocessor' . DIRECTORY_SEPARATOR . 'catalogtableviewtag.php');
+    require_once($site_path . 'tagprocessor' . DIRECTORY_SEPARATOR . 'generaltags.php');
+    require_once($site_path . 'tagprocessor' . DIRECTORY_SEPARATOR . 'pagetags.php');
+    require_once($site_path . 'tagprocessor' . DIRECTORY_SEPARATOR . 'itemtags.php');
+    require_once($site_path . 'tagprocessor' . DIRECTORY_SEPARATOR . 'fieldtags.php');
+}
 
-$jinput=JFactory::getApplication()->input;
+// --------------------- ItemId
+$forceItemId=$params->get('forceitemid');
+if(isset($forceItemId) and $forceItemId!='')
+{
+    //Find Itemid by alias
+    if(((int)$forceItemId)>0)
+        $ct->Env->Itemid=$forceItemId;
+    else
+    {
+        if($forceItemId!="0")
+            $ct->Env->Itemid=(int)JoomlaBasicMisc::FindItemidbyAlias($forceItemId);//Accepts menu Itemid and alias
+        else
+            $ct->Env->Itemid=$ct->Env->jinput->get('Itemid',0,'INT');
+    }
+}
+else
+    $ct->Env->Itemid = $ct->Env->jinput->get('Itemid',0,'INT');
 
-$result='';
+// -------------------- Table
 
-$_params= $params;//new JRegistry;
-JoomlaBasicMisc::prepareSearchFilter($_params);
-		
-$config=array();
-$model = JModelLegacy::getInstance('Catalog', 'CustomTablesModel', $config);
-$model->load($_params, true);
-		
-$pagelayout='{catalog:,notable}';
-		
-$Layouts = new Layouts($model->ct);
+$ct->getTable($params->get( 'establename' ), null);
+
+if($ct->Table->tablename=='')
+{
+    $ct->app->enqueueMessage('Table not selected (185).', 'error');
+    return false;
+}
+
+// --------------------- Filter
+$ct->setFilter('', $ct->Env->menu_params->get('showpublished'));
+$ct->Filter->addMenuParamFilter();
+
+// --------------------- Sorting
+$ct->Ordering->parseOrderByParam(true,$params,$ct->Env->Itemid);
+
+// --------------------- Limit
+$ct->applyLimits(true);
+
+
+// --------------------- Layouts
+$Layouts = new Layouts($ct);
 		
 if($params->get( 'ct_pagelayout' )!=null)
 {
@@ -56,27 +84,31 @@ if($params->get( 'ct_pagelayout' )!=null)
 	if($pagelayout=='')
 		$pagelayout='{catalog:,notable}';
 }
+else
+    $pagelayout='{catalog:,notable}';
 	
 if($params->get( 'ct_itemlayout' )!=null)
 	$itemlayout=$Layouts->getLayout($params->get( 'ct_itemlayout' ));
 else
 	$itemlayout='';
-		
-$model->getSearchResult();
-$catalogtablecode=JoomlaBasicMisc::generateRandomString();//this is temporary replace place holder. to not parse content result again
 
-$catalogtablecontent=tagProcessor_CatalogTableView::process($model->ct,$pagelayout,$catalogtablecode);
-if($catalogtablecontent=='')
-{
-	$LayoutProc = new LayoutProcessor($model->ct);
-    $LayoutProc->layout=$itemlayout;
-	$catalogtablecontent=tagProcessor_Catalog::process($model->ct,$pagelayout,$catalogtablecode);
+// -------------------- Load Records
+$ct->getRecords();
+
+// -------------------- Parse Layouts
+if ($ct->Env->legacysupport) {
+    $catalogtablecode = JoomlaBasicMisc::generateRandomString();//this is temporary replace place holder. to not parse content result again
+
+    $catalogtablecontent = tagProcessor_CatalogTableView::process($ct, $Layouts->layouttype, $pagelayout, $itemlayout, $catalogtablecode);
+    if ($catalogtablecontent == '')
+        $catalogtablecontent = tagProcessor_Catalog::process($ct, $Layouts->layouttype, $pagelayout, $itemlayout, $catalogtablecode);
+
+    $LayoutProc = new LayoutProcessor($ct);
+    $LayoutProc->layout = $pagelayout;
+    $pagelayout = $LayoutProc->fillLayout(array(), null, '');
+    $pagelayout = str_replace('&&&&quote&&&&', '"', $pagelayout); // search boxes may return HTMl elemnts that contain placeholders with quotes like this: &&&&quote&&&&
+    $pagelayout = str_replace($catalogtablecode, $catalogtablecontent, $pagelayout);
 }
-
-$LayoutProc->layout=$pagelayout;
-$pagelayout=$LayoutProc->fillLayout(array(), null, '');
-$pagelayout=str_replace('&&&&quote&&&&','"',$pagelayout); // search boxes may return HTMl elemnts that contain placeholders with quotes like this: &&&&quote&&&&
-$pagelayout=str_replace($catalogtablecode,$catalogtablecontent,$pagelayout);
 
 if($params->get( 'allowcontentplugins' )==1)
 	JoomlaBasicMisc::applyContentPlugins($pagelayout);
