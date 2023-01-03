991104067df89e315d564916721e766baa3211e9
JoomlaBoat <ivankomlev@gmail.com>
2023-01-03 11:37:51 -0500

    Version and Year changed.

--- mod_ctcatalog.php
+++ mod_ctcatalog.php
@@ -4,7 +4,7 @@
  * @package mod_ctcatalog.php
  * @author Ivan Komlev <support@joomlaboat.com>
  * @link http://www.joomlaboat.com
- * @copyright (C) 2018-2022 Ivan Komlev
+ * @copyright (C) 2018-2023 Ivan Komlev
  * @license GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
  **/
 
