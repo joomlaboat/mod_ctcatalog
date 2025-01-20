a8a6eb1a36e1a652d69023067cd0763310d13c4b
joomlaboat <ivankomlev@gmail.com>
2025-01-20 14:52:32 -0900

    Catalog rendering bug fixed.

--- mod_ctcatalog.php
+++ mod_ctcatalog.php
@@ -14,6 +14,8 @@ defined('_JEXEC') or die('Restricted access');
 use CustomTables\CT;
 use CustomTables\Catalog;
 use CustomTables\Params;
+use CustomTables\common;
+use Joomla\CMS\Factory;
 
 $path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_customtables' 
 	. DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'customtables' . DIRECTORY_SEPARATOR;
@@ -21,12 +23,26 @@ $path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 
 require_once($path.'loader.php');
 CustomTablesLoader();
 
-//$params_array = Params::menuParamsRegistry2Array($params);
-
-//$ct = new CT($params_array, true, $module->id);
 $ct = new CT([], true);
+
 $ct->Params->constructJoomlaParams($module->id);
 
 $ctCatalog = new Catalog($ct); //$params is the parameter passed by joomla to the module, it contains module settings
+$result = $ctCatalog->render();
 
-echo $ctCatalog->render();
+if($ct->Table === null) {
+	Factory::getApplication()->enqueueMessage('Custom Tables - Catalog Module: Table not found');
+}
+else {
+	common::loadJSAndCSS($ct->Params, $ct->Env, $ct->Table->fieldInputPrefix);
+
+	$app = Factory::getApplication();
+
+	if (!empty($ctCatalog->layoutCodeCSS))
+		$app->getDocument()->addCustomTag('<style>' . $ctCatalog->layoutCodeCSS . '</style>');
+
+	if (!empty($ctCatalog->layoutCodeJS))
+		$app->getDocument()->addCustomTag('<script>' . $ctCatalog->layoutCodeJS . '</script>');
+
+	echo $result;
+}
