59c3c7d4285a7f1d41bc2db741763d3c87bc4066
joomlaboat <ivankomlev@gmail.com>
2025-06-11 15:13:47 -0800

    Apply URL Query Parameters option added.

--- mod_ctcatalog.php
+++ mod_ctcatalog.php
@@ -8,7 +8,7 @@
  * @license GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
  **/
 
- // no direct access
+// no direct access
 defined('_JEXEC') or die('Restricted access');
 
 use CustomTables\CT;
@@ -18,15 +18,19 @@ use CustomTables\common;
 use Joomla\CMS\Factory;
 use Joomla\Registry\Registry;
 
-$path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_customtables' 
+$path = JPATH_SITE . DIRECTORY_SEPARATOR . 'components' . DIRECTORY_SEPARATOR . 'com_customtables'
 	. DIRECTORY_SEPARATOR . 'libraries' . DIRECTORY_SEPARATOR . 'customtables' . DIRECTORY_SEPARATOR;
 
-require_once($path.'loader.php');
+require_once($path . 'loader.php');
 CustomTablesLoader();
 
-$ct = new CT([], true);
 $menu_params = new Registry;//Joomla Specific
 $menu_params->loadString($module->params);
+
+$applyURLQueryParameters = (int)$menu_params->get('applyurlqueryparameters');
+
+$ct = new CT([], !(bool)$applyURLQueryParameters);
+
 $menu_paramsArray = Params::menuParamsRegistry2Array($menu_params);
 $ct->Params->setParams($menu_paramsArray);
 $ct->Params->ModuleId = $module->id;
@@ -34,10 +38,9 @@ $ct->Params->ModuleId = $module->id;
 $ctCatalog = new Catalog($ct); //$params is the parameter passed by joomla to the module, it contains module settings
 $result = $ctCatalog->render();
 
-if($ct->Table === null) {
+if ($ct->Table === null) {
 	Factory::getApplication()->enqueueMessage('Custom Tables - Catalog Module: Table not found');
-}
-else {
+} else {
 	common::loadJSAndCSS($ct->Params, $ct->Env, $ct->Table->fieldInputPrefix);
 
 	$app = Factory::getApplication();
